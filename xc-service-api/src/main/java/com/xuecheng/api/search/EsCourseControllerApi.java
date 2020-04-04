874bcfdb36166359187ad7b7f6cec89ddc5646ae
Chen Rongjie <2362390174@qq.com>
2020-04-05 01:53:22 +0800

    学习页面查询课程计划

--- xc-service-api/src/main/java/com/xuecheng/api/search/EsCourseControllerApi.java
+++ xc-service-api/src/main/java/com/xuecheng/api/search/EsCourseControllerApi.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Api(value = "课程搜索", tags = {"课程搜索"})
 public interface EsCourseControllerApi {
@@ -14,4 +15,7 @@ public interface EsCourseControllerApi {
     @ApiOperation("课程搜索")
     public QueryResponseResult<CoursePub> list(int page, int size,
                                                CourseSearchParam courseSearchParam) throws IOException;
+
+    @ApiOperation("根据id查询课程信息")
+    public Map<String, CoursePub> getall(String id);
 }
